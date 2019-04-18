d0971cbf6281682c481371086890865c97dd693b
Johnatan Tavares <johnatan.tavares@apisys.com.br>
2019-04-18 16:11:48 -0300

    listando produtos do banco

--- views/index.php
+++ views/index.php
@@ -1,5 +1,30 @@
 <?php require_once __DIR__. '/template/header.php'; ?>
 
-<h1>Hello, world!</h1>
+<div class="main">
+    <div class="container">
+        <h2 class="title">Página de Produtos</h2>
+
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Preço</th>
+                <th scope="col">Descrição</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($vars['products'] as $product) { ?>
+                    <tr>
+                    <th scope="row"><?php echo $product->id ?></th>
+                    <td><?php echo $product->name ?></td>
+                    <td>R$ <?php echo number_format($product->price, 2, ',', '.') ?></td>
+                    <td><?php echo $product->description ?></td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
+</div>
 
 <?php require_once __DIR__. '/template/footer.php'; ?>
