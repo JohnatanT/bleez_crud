217ac77f74f4d7035cc9c611573b5df65f8a9099
Johnatan Tavares <johnatan.tavares@apisys.com.br>
2019-04-21 10:44:26 -0300

    insert do produto

--- source/Controller/ProductController.php
+++ source/Controller/ProductController.php
@@ -13,4 +13,21 @@ final class ProductController extends Controller
         return self::view('index', ['products' => $products]);
     }
 
+    public function store()
+    {
+        $name = $_POST['name'];
+        $price = $_POST['price'];
+        $description = $_POST['description'];
+        $photo = $_FILES["photo"];
+
+        $data = ['name' => $name, 'price' => $price, 'description' => $description, 'photo' => $photo];
+       
+        if ($products = (new ProductService)->store($data)) {
+            return $this->index();
+        }   
+
+        die('Houve um erro no cadastro');
+        
+    }
+
 }
--- source/Service/ProductService.php
+++ source/Service/ProductService.php
@@ -18,4 +18,48 @@ class ProductService
         return $this->model->get();
     }
 
+    public function store(array $data)
+    {
+        $photo = [];
+        $errors = [];
+        $types = [
+            'image/gif',
+            'image/jpeg',
+            'image/png',
+        ];
+
+        if (! in_array($data['photo']['type'], $types)) {
+            $errors[] = "Tipo da Imagem não aceita! <br>";
+        }
+
+        if (! empty($errors)) {
+            return false;
+        }
+
+        switch ($data['photo']['type']) {
+            case 'image/gif':
+                $photo['type'] = 'gif';
+                break;
+            case 'image/jpeg':
+                $photo['type'] = 'jpeg';
+                break;
+            case 'image/png':
+                $photo['type'] = 'png';
+                break;
+        }
+
+         $photo['name'] = md5(uniqid(time())) . '.' . $photo['type'];
+         $dir =  $_SERVER['DOCUMENT_ROOT'] . "/img/" .$photo['name'];
+
+         move_uploaded_file($data['photo']['tmp_name'], $dir);
+
+         return $this->model->create([
+            'name' => $data['name'],
+            'price' => (int) $data['price'],
+            'description' =>  $data['description'],
+            'image_name' => $photo['name']
+        ]);
+
+    }
+
 }
